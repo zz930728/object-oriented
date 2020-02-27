86923d8a625b26c553170975825ca3ea24f16b17
zz930728 <332194951@qq.com>
2020-02-27 08:43:56 +0800

    hm_class

--- python_inheritance.py
+++ python_inheritance.py
@@ -78,3 +78,47 @@ print()
 
 cat = Cat()
 cat.eat()
+print()
+
+
+# class A:
+#     def __init__(self):
+#         # 成员变量
+#         self.num1 = 100
+#         # 私有变量
+#         self.__num2 = 200
+#
+#     def __test(self):
+#         print("私有方法 %d %d" % (self.num1, self.__num2))
+#
+#
+# class B(A):
+#     def demo(self):
+#         # print("访问父类的私有属性%d" % self.__num2)
+#         self.__test()
+#     pass
+#
+#
+# b = B()
+# # print(b._A__num2)
+# # b.demo()
+# # 子类不能访问父类的私有方法
+# b.demo()
+
+
+class A:
+    def __init__(self):
+        # 成员变量
+        self.num1 = 100
+        # 私有变量
+        self.num2 = 200
+
+    def test(self):
+        print("私有方法 %d %d" % (self.num1, self.__num2))
+
+
+class B(A):
+    def demo(self):
+        # print("访问父类的私有属性%d" % self.__num2)
+        self.__test()
+
